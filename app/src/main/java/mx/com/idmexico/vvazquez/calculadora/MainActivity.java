51cb6433d316f951ec4cec1edc34ff67cdb838d0
Víctor Riojas <victor_riojas@yahoo.com.mx>
2016-06-14 18:32:32 -0500

    Pantalla
    Pantalla de la Calculadora

---
+++ app/src/main/java/mx/com/idmexico/vvazquez/calculadora/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.com.idmexico.vvazquez.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
