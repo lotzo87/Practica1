50f559ff4ac5e7d14ee7dc8abf7fdbc4a3946789
Víctor Riojas <victor_riojas@yahoo.com.mx>
2016-06-15 18:26:58 -0500

    Botones numéricos
    Botones del 0 al 9 y borrado

--- app/src/main/java/mx/com/idmexico/vvazquez/calculadora/MainActivity.java
+++ app/src/main/java/mx/com/idmexico/vvazquez/calculadora/MainActivity.java
@@ -1,13 +1,77 @@
 package mx.com.idmexico.vvazquez.calculadora;
 
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText eText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn0).setOnClickListener(this);
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+        findViewById(R.id.btn5).setOnClickListener(this);
+        findViewById(R.id.btn6).setOnClickListener(this);
+        findViewById(R.id.btn7).setOnClickListener(this);
+        findViewById(R.id.btn8).setOnClickListener(this);
+        findViewById(R.id.btn9).setOnClickListener(this);
+        findViewById(R.id.btnSuma).setOnClickListener(this);
+        findViewById(R.id.btnResta).setOnClickListener(this);
+        findViewById(R.id.btnMult).setOnClickListener(this);
+        findViewById(R.id.btnDiv).setOnClickListener(this);
+        findViewById(R.id.btnMod).setOnClickListener(this);
+        findViewById(R.id.btnRes).setOnClickListener(this);
+        findViewById(R.id.btnDel).setOnClickListener(this);
+
+        eText = (EditText) findViewById(R.id.txtRes);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn0:
+            case R.id.btn1:
+            case R.id.btn2:
+            case R.id.btn3:
+            case R.id.btn4:
+            case R.id.btn5:
+            case R.id.btn6:
+            case R.id.btn7:
+            case R.id.btn8:
+            case R.id.btn9:
+                double d = Double.parseDouble(((Button)findViewById(v.getId())).getText().toString());
+                eText.setText(eText.getText().toString().concat(String.format(Double.toString(d))));
+            break;
+            case R.id.btnDel:
+                if (eText.getText().length() >0)
+                    eText.setText("");
+                break;
+
+        }
+
+
+
     }
 }
