83acbd47f7dfa8d3f1d581848c22e554925d9c04
Víctor Riojas <victor_riojas@yahoo.com.mx>
2016-06-19 22:03:38 -0500

    Operaciones con decimales

--- app/src/main/java/mx/com/idmexico/vvazquez/calculadora/MainActivity.java
+++ app/src/main/java/mx/com/idmexico/vvazquez/calculadora/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
     private EditText eText;
+    private String n1="",n2 ="", oper="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         findViewById(R.id.btnMod).setOnClickListener(this);
         findViewById(R.id.btnRes).setOnClickListener(this);
         findViewById(R.id.btnDel).setOnClickListener(this);
+        findViewById(R.id.btnPto).setOnClickListener(this);
 
         eText = (EditText) findViewById(R.id.txtRes);
     }
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+
         switch (v.getId()){
             case R.id.btn0:
             case R.id.btn1:
@@ -61,17 +64,82 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             case R.id.btn7:
             case R.id.btn8:
             case R.id.btn9:
-                double d = Double.parseDouble(((Button)findViewById(v.getId())).getText().toString());
-                eText.setText(eText.getText().toString().concat(String.format(Double.toString(d))));
+                int d = Integer.parseInt(((Button) findViewById(v.getId())).getText().toString());
+                eText.setText(eText.getText().toString().concat(String.format(Integer.toString(d))));
+                if (oper.equals(""))
+                    n1= String.valueOf(d);
+                else
+                    n2= String.valueOf(d);
             break;
             case R.id.btnDel:
-                if (eText.getText().length() >0)
+                if (eText.getText().length() >0) {
                     eText.setText("");
+                    n1="";
+                    n2="";
+                    oper="";
+                }
                 break;
+            case R.id.btnPto:
+                if (eText.getText().toString().indexOf(".") == -1)
+                    if (eText.getText().toString()== "")
+                        eText.setText(String.format("0").concat("."));
+                    else
+                        eText.setText(String.format(eText.getText().toString().concat(".")));
+
+                break;
+            case R.id.btnSuma:
+                oper= "+";
+                break;
+            case R.id.btnResta:
+                oper= "-";
+                break;
+            case R.id.btnMult:
+                oper="*";
+                break;
+            case R.id.btnDiv:
+                oper ="/";
+                break;
+            case R.id.btnMod:
+                oper="%";
+                break;
+            case R.id.btnRes:
+                if (!(n1.trim().equals(""))) {
+                    eText.setText(calculate(oper, n1, n2.equals("") ? n1 : n2));
+                    n1="";
+                    n2="";
+                    oper="";
+                }
+
 
         }
+    }
 
-
-
+    public String calculate(String oper,String n1, String n2)
+    {
+        double d1 = Double.parseDouble(n1);
+        double d2 = Double.parseDouble(n2);
+        double r=0;
+        switch (oper)
+        {
+            case "+":
+                r= d1+d2;
+                break;
+            case "-":
+                r= d1-d2;
+                break;
+            case "*":
+                r= d1*d2;
+                break;
+            case "/":
+                if (d2!=0)
+                    r= d1/d2;
+                else
+                    return "Error";
+                break;
+            case "%":
+                r= d1%d2;
+                break;
+        }
+        return Double.toString(r);
     }
 }
